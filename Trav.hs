13c59e0a9a309ecc475dddc2acdf066c52f68a9e
XZentus <z_xmn@mail.ru>
2018-04-03 10:27:05 +0300

    Update Trav.hs

--- Trav.hs
+++ Trav.hs
@@ -1,3 +1,5 @@
+module Main where
+
 import Data.Foldable
 import Control.Applicative
 
@@ -16,8 +18,8 @@ printTest s1 s2 a1 a2 = do
     putStrLn s2
     print $ a1 == a2
 
-test :: IO ()
-test =
+testtraverse2list, testTriple :: IO ()
+testtraverse2list =
     traverse_ (unf4 printTest) [( "traverse2list (\\x -> [x+10,x+20]) [1,2,3]"
                                 , "[[11,12,13],[11,12,23],[11,22,13],[11,22,23],[21,12,13],[21,12,23],[21,22,13],[21,22,23]]"
                                 ,  traverse2list (\x -> [x+10,x+20]) [1,2,3]
@@ -29,7 +31,48 @@ test =
                                 ,  [[12,11,13],[12,11,23],[12,21,13],[12,21,23],[22,11,13],[22,11,23],[22,21,13],[22,21,23]]
                                 )
                                ]
-main = test
+
+testTriple = do
+    (unf4 printTest) ( "foldl (++) \"!!\" (Tr \"ab\" \"cd\" \"efg\")"
+                                , "!!abcdefg"
+                                ,  foldl (++) "!!" (Tr "ab" "cd" "efg")
+                                , "!!abcdefg"
+                                )
+    (unf4 printTest) ( "traverse (\\x -> if x>10 then Right x else Left x) (Tr 12 14 16)"
+                                , "Right (Tr 12 14 16)"
+                                ,  traverse (\x -> if x>10 then Right x else Left x) (Tr 12 14 16)
+                                ,  Right (Tr 12 14 16)
+                                )
+    (unf4 printTest) ( "traverse (\\x -> if x>10 then Right x else Left x) (Tr 12 8 4)"
+                                , "Left 8"
+                                ,  traverse (\x -> if x>10 then Right x else Left x) (Tr 12 8 4)
+                                ,  Left 8
+                                )
+    (unf4 printTest) ( "sequenceA (Tr (Tr 1 2 3) (Tr 4 5 6) (Tr 7 8 9))"
+                                , "Tr (Tr 1 4 7) (Tr 2 5 8) (Tr 3 6 9)"
+                                ,  sequenceA (Tr (Tr 1 2 3) (Tr 4 5 6) (Tr 7 8 9))
+                                ,  Tr (Tr 1 4 7) (Tr 2 5 8) (Tr 3 6 9)
+                                )
+
+data Triple a = Tr a a a  deriving (Eq,Show)
+
+instance Functor Triple where
+    fmap f (Tr x y z) = Tr (f x) (f y) (f z)
+
+instance Foldable Triple where
+--  foldr :: (a -> b -> b) -> b -> t a -> b 
+    foldr f ini (Tr x y z) = f x $ f y $ f z ini
+
+instance Applicative Triple where
+    pure x = Tr x x x
+--  (<*>) :: f (a -> b) -> f a -> f b
+    (Tr f1 f2 f3) <*> (Tr v1 v2 v3) = Tr (f1 v1) (f2 v2) (f3 v3)
+
+instance Traversable Triple where
+--  traverse :: Applicative f => (a -> f b) -> t a -> f (t b)
+--  sequenceA :: Applicative f => t (f a) -> f (t a) 
+
+main = testTriple
 
 unf4 :: (a -> b -> c -> d -> e) -> (a, b, c, d) -> e
 unf4 f (a, b, c, d) = f a b c d
