78c6f93818ee61b4e4b33fdb7e61f7fef8fb99df
XZentus <z_xmn@mail.ru>
2018-10-28 20:52:08 +0300

    Update e27.hs

--- e27.hs
+++ e27.hs
@@ -2,25 +2,38 @@ import Control.Monad
 import Control.Monad.State.Strict
 import Data.Foldable
 
+data QuadPrimesLen = QPL {a :: Int
+                         ,b :: Int
+                         ,n :: Int
+                         }
+    deriving Show
+
+instance Eq QuadPrimesLen where
+    (QPL _ _ a) == (QPL _ _ b) = a == b
+    (QPL _ _ a) /= (QPL _ _ b) = a /= b
+
+instance Ord QuadPrimesLen where
+    compare (QPL _ _ a) (QPL _ _ b) = a `compare` b
+
+getPrimeSeqLen :: Int -> Int -> State [Int] QuadPrimesLen
+getPrimeSeqLen a b = do
+    ps <- get
+    let (newPs, result) = check ps
+    put newPs
+    return $ QPL a b result
+  where
+    check :: [Int] -> ([Int], Int)
+    check = undefined
+
 primesInit :: [Int]
-primesInit = reverse [2, 3, 5, 7, 11, 13, 17]
+primesInit = [2, 3, 5, 7, 11, 13, 17, 19, 23]
 
-extendPrimes :: Int -> [Int] -> [Int]
-extendPrimes n (p:ps) = undefined
-
-getPrimeSeqLen :: undefined
-
-maxCoeffs :: [Int] -> [Int] -> [(Int, Int, Int)]
-maxCoeffs as bs = [(a, b, getPrimeSeqLen a b) | a <- as, b <- bs]
-
-get3 :: (a, b, c) -> c
-get3 (_, _, c) = c
-
-routine :: Int -> Int -> State [Int] Int
-routine a b = do
+uncurry3 :: (a -> b -> c -> d) -> (a, b, c) -> d
+uncurry3 f (a, b, c) = f a b c
 
 main :: IO ()
 main = do
---  let range = [-999 .. 999]
---  print $ get3 $ foldl' (\f s -> if get3 f > get3 s
---                                 then f else s) $ maxCoeffs range range
+    let range = [-999 .. 999]
+        primesSeqList = flip evalState primesInit $ sequence $ zipWith getPrimeSeqLen range range
+        test = map (uncurry3 QPL) [(1, 2, 3), (3, 4, 5), (10, 20, 0), (-100, 100, 100)]
+    print $ foldl' max (head test) (tail test)
