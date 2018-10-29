9bc0089dfc3a53495a67e5ca02906dbf07bce969
XZentus <z_xmn@mail.ru>
2018-10-29 09:35:18 +0300

    Update e27.hs

--- e27.hs
+++ e27.hs
@@ -1,10 +1,9 @@
-import Control.Monad
 import Control.Monad.State.Strict
 import Data.Foldable
 
-data QuadPrimesLen = QPL {a :: Int
-                         ,b :: Int
-                         ,n :: Int
+data QuadPrimesLen = QPL { a :: Int
+                         , b :: Int
+                         , n :: Int
                          }
     deriving Show
 
@@ -28,24 +27,23 @@ getPrimeSeqLen a b = do
                      then check nps' $ n + 1
                      else (nps', n)
     isPrime :: [Int] -> Int -> ([Int], Bool)
-    isPrime p       n | n < 0      = (,) p False
-    isPrime p@(f:_) n | f * f == n = (,) p False
-                      | f * f >  n = let result = all (\d -> (n `rem` d) /= 0) p
-                                     in (,) p result
-                      | f * f <  n = let nps = getNextPrime p
-                                     in isPrime nps n
+    isPrime p@(f:_) n | n < 0 || f2 == n = (,) p False
+                      | f2 >  n          = let result = all (\d -> (n `rem` d) /= 0) p
+                                           in (,) p result
+                      | otherwise        = let nps = getNextPrime p
+                                           in isPrime nps n
+      where f2 = f * f
+    isPrime [] _                         = error "getPrimeSeqLen -> check -> isPrime: No primes list to check"
     getNextPrime :: [Int] -> [Int]
     getNextPrime p@(f:_) = gnp (f + 2) p
       where
         gnp n [] = n : p
         gnp n (f:rest) | n `rem` f == 0 = gnp (n + 2) p
                        | otherwise      = gnp n rest
+    getNextPrime [] = error "getPrimeSeqLen -> check -> isPrime -> getNextPrime: No primes list"
 
 primesInit :: [Int]
 primesInit = reverse [2, 3, 5, 7, 11, 13, 17, 19, 23]
-
-uncurry3 :: (a -> b -> c -> d) -> (a, b, c) -> d
-uncurry3 f (a, b, c) = f a b c
 
 main :: IO ()
 main = do
@@ -53,7 +51,8 @@ main = do
         bRange = [-1000 .. 1000]
         raw = flip runState primesInit $ sequence $ [getPrimeSeqLen a b | a <- aRange, b <- bRange]
         primesSeqList = fst raw
---        test = map (uncurry3 QPL) [(1, 2, 3), (3, 4, 5), (10, 20, 0), (-100, 100, 100)]
+--        test = map (uncurry3 QPL) [(1, 2, 3), (3, 4, 5), (10, 20, 0), (-100, 100, 100)] where
+--          uncurry3 f (a, b, c) = f a b c
         result = foldl' max (head primesSeqList) (tail primesSeqList)
 --    print $ foldl' max (head test) (tail test)
     print $ snd $ raw
